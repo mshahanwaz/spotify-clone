ac5c414be7c39fbbac370bf7e3db2f012b9fbdd6
mshahanwaz <mohdshahanwaz55@gmail.com>
2021-01-28 23:42:10 +0530

    Added screenshots

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBnAOhhQZ7gyc_Wp_Ig089ju4aS1j1NGc4",
+  authDomain: "spotify-clone-2a337.firebaseapp.com",
+  projectId: "spotify-clone-2a337",
+  storageBucket: "spotify-clone-2a337.appspot.com",
+  messagingSenderId: "720571401524",
+  appId: "1:720571401524:web:642c8d161b3bbad895d161",
+  measurementId: "G-PGN8J34VP0"
+});
+
